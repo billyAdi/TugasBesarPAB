f9cd71a5b7f509c0dc61c362fd81147a001d9661
Ferdinandus Renaldi <ferdinandus.renaldi@gmail.com>
2018-05-10 19:06:30 +0700

    Create Lingkaran.java

---
+++ app/src/main/java/com/example/user/tugasbesarpab/Lingkaran.java
@@ -0,0 +1,67 @@
+package com.example.user.tugasbesarpab;
+
+/**
+ * Created by WIN 10 on 5/10/2018.
+ */
+
+public class Lingkaran {
+    public int posX,posY,rad,speedX,speedY;
+
+    public int getPosX() {
+        return posX;
+    }
+
+    public void setPosX(int posX) {
+        this.posX = posX;
+    }
+
+    public int getPosY() {
+        return posY;
+    }
+
+    public void setPosY(int posY) {
+        this.posY = posY;
+    }
+
+    public int getRad() {
+        return rad;
+    }
+
+    public void setRad(int rad) {
+        this.rad = rad;
+    }
+
+    public int getSpeedX() {
+        return speedX;
+    }
+
+    public void setSpeedX(int speedX) {
+        this.speedX = speedX;
+    }
+
+    public int getSpeedY() {
+        return speedY;
+    }
+
+    public void setSpeedY(int speedY) {
+        this.speedY = speedY;
+    }
+
+    public Lingkaran(int posX, int posY, int rad, int speedX, int speedY) {
+
+        this.posX = posX;
+        this.posY = posY;
+        this.rad = rad;
+        this.speedX = speedX;
+        this.speedY = speedY;
+    }
+
+    public Lingkaran(int posX, int posY, int rad) {
+
+        this.posX = posX;
+        this.posY = posY;
+        this.rad = rad;
+        this.speedX = 0;
+        this.speedY = 0;
+    }
+}
