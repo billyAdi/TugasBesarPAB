cbfb516aa925d85325e19fe0b4620bc0717b3419
billyAdi <billz.wew@gmail.com>
2018-05-12 14:33:12 +0700

    spinner lengkap

--- app/src/main/java/com/example/user/tugasbesarpab/SettingsFragment.java
+++ app/src/main/java/com/example/user/tugasbesarpab/SettingsFragment.java
@@ -5,12 +5,15 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 /**
  * Created by user on 4/30/2018.
  */
 
 public class SettingsFragment extends Fragment  {
+    protected Spinner spinnerSpeed,spinnerColor1,spinnerColor2,spinnerBonus;
 
 
     public SettingsFragment() {
@@ -25,6 +28,36 @@ public class SettingsFragment extends Fragment  {
     @Override
     public View onCreateView(LayoutInflater inflater,  ViewGroup container,  Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.settings_fragment,container,false);
+
+
+        this.spinnerSpeed=view.findViewById(R.id.spinner_speed);
+        this.spinnerColor1=view.findViewById(R.id.spinner_color1);
+        this.spinnerColor2=view.findViewById(R.id.spinner_color2);
+        this.spinnerBonus=view.findViewById(R.id.spinner_bonus);
+
+        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(getContext(),
+                R.array.speedArray, android.R.layout.simple_spinner_item);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(getContext(),
+                R.array.color1Array, android.R.layout.simple_spinner_item);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        ArrayAdapter<CharSequence> adapter3 = ArrayAdapter.createFromResource(getContext(),
+                R.array.color2Array, android.R.layout.simple_spinner_item);
+        adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        ArrayAdapter<CharSequence> adapter4 = ArrayAdapter.createFromResource(getContext(),
+                R.array.bonusArray, android.R.layout.simple_spinner_item);
+        adapter4.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        this.spinnerSpeed.setAdapter(adapter1);
+        this.spinnerColor1.setAdapter(adapter2);
+        this.spinnerColor2.setAdapter(adapter3);
+        this.spinnerBonus.setAdapter(adapter4);
+
+
+
         return view;
     }
 
